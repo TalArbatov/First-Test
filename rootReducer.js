c2de210f4c300db8298dccb3fd39313686fa9b46
TalArbatov <talarbatov98@gmail.com>
2018-07-03 21:33:59 +0300

    deleted ADD_PERSON

--- rootReducer.js
+++ rootReducer.js
@@ -5,14 +5,11 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   const newState = {...state};
   switch(action.type) {
-    case actions.ADD_PERSON:
-      newState = {...state, personList: [...state.personList].concat(action.payload.person)};
-      break;
     case: actions.DELETE_PRSON:
       newState = {...state, personList: [...state.personList].filter(person => person.name !== action.payload.name)};        
       break;
      default:
       break;
-  }
+  }						
   return newState;
 }
